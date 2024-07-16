e20485d3d20a8a2bf0d1cb45dff1c26d149af022
Aakash malviya <malviyaaakash4509@gmail.com>
2024-07-16 11:59:37 +0530

    mongodb connection

---
+++ src/db/connection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { db_name } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${db_name}}`);
+        console.log("run success", connectionInstance.connection.host);
+    } catch (error) {
+        console.log("Error:", error);
+        process.exit(1)
+        throw error;
+    }
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-    
+import connectDB from "./db/connection.js";
+import 'dotenv/config'
+import express from "express";
+const app = express();
+connectDB()
+.then(()=>{
+        app.on("error",(err)=>{
+            console.log("Error in express on:", err);
+        })
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log("Server Is Start");
+        })
+})
+.catch((err)=>{
+    console.log("MongoDb connection failed !!", err);
+})
